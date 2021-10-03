378ad101a4a7ee48a98806d7dcb08d6c52941427
Gaëtan <blaxoujunior@gmail.com>
2021-10-03 19:00:26 +0200

    lots of improvements

--- extract_rules/README.md
+++ extract_rules/README.md
@@ -1,1 +1,5 @@
-A simple command-line application.
+# All lint rules
+
+To use, add `all_lints_rules` to your dev dependencies with `dart pub add all_lints_rules --dev`.
+
+Then, include it in your analysis_options.yaml by adding `include: package:all_lint_rules/all.yaml` in the top of your file.
--- extract_rules/bin/extract_rules.dart
+++ extract_rules/bin/extract_rules.dart
@@ -1,19 +1,44 @@
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:yaml/yaml.dart';
 
 final regex = RegExp(r"<code>(.*)<\/code>", dotAll: true);
 
 void main(List<String> arguments) async {
   final file = File(arguments.first);
   if (!file.existsSync()) throw Exception("File not found");
+  print("Target file: $file");
 
   var url = Uri.parse(
       "https://dart-lang.github.io/linter/lints/options/options.html");
   final res = await http.get(url);
 
-  final m = regex.firstMatch(res.body)?.group(1);
-  if (m == null) throw Exception("Unable to parse $url");
+  final match = regex.firstMatch(res.body)?.group(1);
+  if (match == null) throw Exception("Unable to parse $url");
 
-  await file.writeAsString(m);
+  final string = """
+# Generated on ${DateFormat.yMMMMd().add_Hm().format(DateTime.now().toUtc())} UTC
+
+$match
+  """;
+
+  print("YAML: ");
+  print("=" * 20);
+  print(string);
+
+  final yaml = loadYaml(string);
+
+  try {
+    if (yaml["linter"]["rules"] == null) {
+      throw FormatException("Invalid yaml file", yaml);
+    }
+  } on NoSuchMethodError {
+    throw FormatException("Invalid yaml file", yaml);
+  }
+
+  print("Writing to $file...");
+  await file.writeAsString(string);
+  print("Done writing !");
 }
